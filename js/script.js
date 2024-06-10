dd1d44f6a502a34d96d083ea40c30bdeeab0ebc5
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-06-10 17:36:23 +0000

    done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+async function getDogImage() {
+
+  try {
+    const resultJSON = await fetch("https://dog.ceo/api/breeds/image/random")
+    const jsonData = await resultJSON.json()
+    
+    const firstDataSet = jsonData.message
+
+    document.getElementById("dog-image").src = firstDataSet
+  } catch (error) {
+    console.error(error)
+  }
+}
